807955a5a173b2a5702498855424b1dd30a55c34
Manu <manu@gmail.com>
2025-11-05 22:49:24 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PortfolioComponent],
+  imports: [ PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
